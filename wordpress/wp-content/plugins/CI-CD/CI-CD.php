f3f48b9f3fd67c6d59e6d39a00dbc1ab0577dce8
Ubuntu <ubuntu@ip-172-31-27-11.ec2.internal>
2025-12-08 21:26:06 +0000

    Prueba edicion CI-CD

---
+++ wordpress/wp-content/plugins/CI-CD/CI-CD.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: CI-CD
+Plugin URI: http://98.81.83.79:80/
+Description: Plugin de prueba para CI/CD
+Version: 1.0
+Author: Tu Nombre
+Author URI: http://98.81.83.79:80/
+*/
+
+function cicd_saludo() {
+    echo '<p>Hola! Este es mi plugin CI-CD activo.</p>';
+}
+
+add_action('wp_footer', 'cicd_saludo');
